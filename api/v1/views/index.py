d1234230f8ee8388e867c80857ffcf0f4a67a22d
Dannyelgeek <daot1993@gmail.com>
2023-04-11 15:18:19 -0500

    AirBnB clone: RESTful API task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,17 +12,22 @@ from models.state import State
 from models.user import User
 
 
-
 @app_views.route('/status')
 def return_ok():
     '''return status OK'''
     return jsonify(status='OK')
 
+
 @app_views.route('/api/v1/stats')
 def count_obj():
     ''''''
-    classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+    classes = {"Amenity": Amenity,
+               "BaseModel": BaseModel,
+               "City": City,
+               "Place": Place,
+               "Review": Review,
+               "State": State,
+               "User": User}
     dict_obj = {}
 
     for cls_name, cls_count in classes.items():
