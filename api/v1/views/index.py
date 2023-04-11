2f537c8f276e95ca704588d10634d2c444bdbe55
Dannyelgeek <daot1993@gmail.com>
2023-04-11 14:35:49 -0500

    AirBnB clone: RESTful API task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,8 @@
 '''index.py documentation.'''
 from api.v1.views import app_views
 
+
 @app_views.route('/status')
 def return_ok():
-    status_ok = {
-    "status": "OK"
-    }
+    status_ok = {"status": "OK"}
     return status_ok.json()
