bdb502a3853b561c9193b6d93f827be66334f59f
Dannyelgeek <daot1993@gmail.com>
2023-04-11 12:59:31 -0500

    AirBnB clone: RESTful API task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,14 +9,16 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def handler():
     '''calls storage.close()'''
     storage.close()
 
+
 if __name__ == '__main__':
     app.run(
-        host= getenv('HBNB_API_HOST', '0.0.0.0'),
-        port= getenv('HBNB_API_PORT', '5000'),
-        threaded=True
-        )
+            host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'),
+            threaded=True
+            )
