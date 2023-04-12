4c1fcbf148b20264922b2096db7fd3e9f24e696b
Dannyelgeek <daot1993@gmail.com>
2023-04-12 18:55:23 -0500

    AirBnB clone: RESTful API task 11

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -15,8 +15,8 @@ def place_list(city_id):
     ct = storage.get(City, city_id)
     if not ct:
         abort(404)
-    pl_list = [pl_list.to_dict() for pl_list in ct.pl_list]
-    return jsonify(pl_list)
+    places = [places.to_dict() for places in ct.places]
+    return jsonify(places)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'],
@@ -26,21 +26,19 @@ def place_obj(place_id):
     pl = storage.get(Place, place_id)
     if pl:
         return jsonify(pl.to_dict())
-    else:
-        abort(404)
+    abort(404)
 
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
     '''Deletes a City object'''
-    empty_dict = {}
     pl = storage.get(Place, place_id)
     if not pl:
         abort(404)
     pl.delete()
     storage.save()
-    return jsonify(empty_dict), 200
+    return jsonify(), 200
 
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
@@ -50,20 +48,25 @@ def new_place(city_id):
     ct = storage.get(City, city_id)
     if not ct:
         abort(404)
+
     new_request = request.get_json()
     if not new_request:
-        abort(400, description='Not a JSON')
+        abort(400, 'Not a JSON')
+
     if 'user_id' not in new_request.keys():
-        abort(400, description='Missing user_id')
-    user_vl = storage.get(User, new_request['user_id'])
-    if not user_vl:
+        abort(400, 'Missing user_id')
+
+    valid_user = storage.get(User, new_request['user_id'])
+    if not valid_user:
         abort(404)
+
     if 'name' not in new_request.keys():
         abort(400, 'Missing name')
-    place_recent = Place(**new_request)
-    setattr(place_recent, 'city_id', city_id)
-    place_recent.save()
-    return jsonify(place_recent.to_dict()), 201
+
+    new_pl = Place(**new_request)
+    setattr(new_pl, 'city_id', city_id)
+    new_pl.save()
+    return jsonify(new_pl.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'],
@@ -73,12 +76,15 @@ def place_now(place_id):
     pl = storage.get(Place, place_id)
     if not pl:
         abort(404)
+
     new_request = request.get_json()
     if not new_request:
-        abort(400, description='Not a JSON')
+        abort(400, 'Not a JSON')
+
     for key, value in new_request.items():
         if key in ['id', 'created_at', 'updated_at']:
             continue
-        setattr(pl, key, value)
+        else:
+            setattr(pl, key, value)
     storage.save()
-    return make_response(jsonify(pl.to_dict())), 200
+    return make_response(jsonify(pl.to_dict()), 200)
