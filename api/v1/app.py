078e309ac486c51ffd4aed7265571c33bc18aacb
Dannyelgeek <daot1993@gmail.com>
2023-04-12 18:46:24 -0500

    AirBnB clone: RESTful API task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r'/*': {'origins':'http://0.0.0.0'}})
+CORS(app, resources={'/*': {'origins': 'http://0.0.0.0'}})
 
 
 @app.teardown_appcontext
@@ -27,7 +27,7 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.run(
-            host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', '5000')),
-            threaded=True
-            )
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=int(getenv('HBNB_API_PORT', '5000')),
+        threaded=True
+    )
