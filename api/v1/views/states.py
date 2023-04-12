b46757e4d5c12204450ca4baaef349f4264b1e07
Dannyelgeek <daot1993@gmail.com>
2023-04-12 09:03:05 -0500

    AirBnB clone: RESTful API task 7

---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+'''state app connect file.'''
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+from flask import jsonify, abort, request, make_response
+
+
+@app_views.route('/states/', methods=['GET'], strict_slashes=False)
+def states_list():
+    '''Retrieves the list of all State objects'''
+    st_list = []
+    for st in storage.all(State).values():
+        st_list.append(st.to_dict())
+    return jsonify(st_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state_obj(state_id):
+    '''Retrieves a State object'''
+    for ind in storage.all(State).values():
+        if ind.id == state_id:
+            return jsonify(ind.to_dict())
+        else:
+            abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    '''Deletes a State object'''
+    empty_dict = {}
+    st = storage.get(State, state_id)
+    if not st:
+        abort(404)
+    else:
+        st.delete()
+        st.save()
+    return jsonify(empty_dict), 200
+
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def new_state():
+    '''Creates a State'''
+    new_request = request.get_json()
+    if not new_request:
+        abort(404, description='Not a JSON')
+    if 'name' not in new_request.keys():
+        abort(404, description='Missing name')
+    state = State(**new_request)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def uodate_state(state_id):
+    '''Updates a State object'''
+    st = storage.get(State, state_id)
+    if not st:
+        abort(404)
+    new_request = request.get_json()
+    if not new_request:
+        abort(404, description='Not a JSON')
+    for key, value in new_request.items():
+        if key in ['id', 'created_at', 'updated_at']:
+            continue
+        setattr(st, key, value)
+    st.save()
+    return make_response(jsonify(st.to_dict())), 200
